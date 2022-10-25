6c888bb7079c4d16ac9fa2952406973893801d69
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-10-25 23:15:15 +0300

    2022.10.25_вечер

---
+++ src/Lab7/Airplane.java
@@ -0,0 +1,27 @@
+package Lab7;
+
+import java.util.Arrays;
+
+public class Airplane
+{
+    public static void main(String[] args) {
+        int [][] Array = {{5,7,3,17}, {7,0,1,12}};
+        System.out.println(Arrays.toString(Array));
+    }
+
+    static String name = "Boing";
+//    static String name = "AirBas";
+
+    public class Wing // вложенный класс
+    {
+        private int weight; // поле для хранения информации про вес крыла
+
+
+
+
+        void wingWeight() // метод показывающий вес крыла
+        {
+
+        }
+    }
+}
---
+++ src/Lab7/Main.java
@@ -0,0 +1,29 @@
+package Lab7;
+
+import java.util.Scanner; // позволяет использовать класс Scanner
+
+public class Main {
+    public static void main(String[] args) {
+        // массив хранения названий самолетов
+        String[] airplanesName = {"Airbus", "Boing", "B-52", "F-35"};
+        // массив хранения весовых характеристик крыльев
+        int[] wingWeightDB = {10000, 15000, 5000, 1500};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название самолета - на выбор: \"Airbus\", \"Boing\", \"B-52\", \"F-35\"");
+
+        String target = scanner.nextLine();
+
+        // получаем индекс самолета
+        boolean m = true;
+        for (int i = 0; i < airplanesName.length; i++) {
+            if (m = (target.equals(airplanesName[i]))) {
+                m = true;
+            }
+            if (m)
+                System.out.println("номер самолета в массиве " + i);
+            // по индексу самолета получаем вес крыла
+        }
+        System.out.println("вес крыла самолета " + wingWeightDB[2]);
+    }
+}
