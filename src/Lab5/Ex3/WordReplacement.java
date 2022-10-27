6f63a6a10f8dd379ca5c77875d24ba1db1696ac3
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-10-27 22:33:21 +0300

    2022.10.26_вечер

--- src/Lab5/Ex3/WordReplacement.java
+++ src/Lab5/Ex3/WordReplacement.java
@@ -1,39 +1,41 @@
 package Lab5.Ex3;
 
-import java.util.Arrays;
-
 // у.3.	Написать метод, заменяющий в тексте все вхождения слова «бяка» на «[вырезано цензурой]».
 
 public class WordReplacement {
     public static void main(String[] args) {
-        String text = "  Нап,исать. Метод, бяка неоБХОдимыЙ,, БяКА для пои.сКа самОГО длиНн.ого сл,ова в ТекстE . ";
-        System.out.println(text); // выводим неформатированный текст в строку
 
-        longWord(text);
+        String text = "Напишите метод, заменяющий в тексте все вхождения слова бяка на [вырезано цензурой]";
+        System.out.println(text); // выводим неформатированный текст в строку
+        System.out.println(" "); // пробел
+        String biaka = "бяка";
+        String noBiaka = "[вырезано цензурой]";
+
+        wrc(text, biaka, noBiaka);
     }
-    
-    public static void longWord(String text) // метод принимает на вход массив
+
+    public static void wrc (String text, String biaka, String noBiaka) // метод принимает на вход 3 строки
     {
         text = text.replace(",", ""); // убираем запятые
         text = text.replace(".", ""); // убираем запятые
         text = text.trim(); // убираем пробелы спереди и сзади
         text = text.toLowerCase(); // приводим к нижнему регистру
+        System.out.println(text); // выводим форматированный текст в строку
+        System.out.println(" "); // пробел
+
         String[] array = text.split(" "); // разбиение на слова по пробелу
-
-        System.out.println(Arrays.toString(array)); // вывод массива в строку
-        System.out.println("Массив содержит " + array.length + " элементов"); // определяем сколько элементов содержит массив
-
-        String target = "бяка";
-        boolean m = true;
+        StringBuilder newText = new StringBuilder();
         for (int i = 0; i < array.length; i++) {
-            if (m = (target.equals(array[i]))) {
-                m = true;
+            if (array[i].equals(biaka)) {
+                array[i] = noBiaka;
             }
-            if (m) {
-                System.out.println("Индекс нецензурного слова: " + i);
-                System.out.println(array[i]);
-            }
+            newText.append(array[i]);
+            newText.append(" ");
         }
+        System.out.println(newText);
     }
 }
+
+
+
 
