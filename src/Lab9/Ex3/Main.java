89823cf022343e70c65c8b004325da33a3f57305
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-11-03 21:52:40 +0300

    2022.11.03_вечер

--- src/Lab9/Ex3/Main.java
+++ src/Lab9/Ex3/Main.java
@@ -1,8 +1,35 @@
 package Lab9.Ex3;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+        User user4 = new User();
+        User user5 = new User();
 
-        User.Game(); // запуск
+        user1.setName("Suvi");
+        user2.setName("Juha");
+        user3.setName("Kaija");
+        user4.setName("Lauri");
+        user5.setName("Pekka");
+
+        // Мапа для хранения со случайным заполнением очков
+        Map<User, Integer> gameResult = new HashMap<>();
+
+        gameResult.put(user1, (int)(Math.random()*(1000)));
+        gameResult.put(user2, (int)(Math.random()*(1000)));
+        gameResult.put(user3, (int)(Math.random()*(1000)));
+        gameResult.put(user4, (int)(Math.random()*(1000)));
+        gameResult.put(user5, (int)(Math.random()*(1000)));
+
+//        System.out.println(gameResult);
+
+        User.tableResults(gameResult); // запуск метода
     }
 }
+
--- src/Lab9/Ex3/User.java
+++ src/Lab9/Ex3/User.java
@@ -1,48 +1,32 @@
 package Lab9.Ex3;
 
-import Lab7.Airplane_v2;
-
-import java.util.*;
+import java.util.Map;
+import java.util.Scanner;
 
 public class User {
     private String name;
 
-    public User(String name) // конструктор
-    {
+    public User() // конструктор
+    {}
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getName() // геттер
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name) // сеттер
-    {
-        this.name = name;
-    }
-
-    public static void Game() {
-        Map<String, Integer> Follout4 = new HashMap<>();
-
-        // Map для хранения результатов игры Follout4
-        Follout4.put("Juha", 12345);
-        Follout4.put("Suvi", 1254);
-        Follout4.put("Annika", 1445425);
-        Follout4.put("Samuli", 15626);
-        Follout4.put("Jenni", 124456);
-
-        System.out.println(""); // пробел
-        System.out.println("Таблица результатов игроков для сравнения:"); // пробел
-        System.out.println(Follout4);
-        System.out.println(""); // пробел
-
+    public static void tableResults(Map<User, Integer> gameResult) {
+        System.out.println(" "); // пробел
+        System.out.println("Введите имя игрока: \"Suvi\", \"Juha\", \"Kaija\", \"Lauri\" или \"Pekka\"");
         Scanner scanner = new Scanner(System.in);
-        // По факту имя пользователя - это "ключ"
-        System.out.println("Напишите имя пользователя: \"Juha\", \"Suvi\", \"Annika\", \"Samuli\" , \"Jenni\"");
-        String target = scanner.nextLine(); // ввести цель для сравнения в коде
-
-        Integer integer = Follout4.get(target);
-        System.out.println(target + " " + integer); // получение количества очков по имени игрока
+        String console = scanner.nextLine();
+        for(Map.Entry<User, Integer> entry : gameResult.entrySet()){
+            if(entry.getKey().getName().equals(console)){
+                System.out.println(console + " - " + entry.getValue() + " очков");
+            }
+        }
     }
+
 }
